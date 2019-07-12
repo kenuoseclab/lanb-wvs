a4354cca68c492f774dc92c665bd751c230f6b5a
colodoo <lodog1@qq.com>
2019-07-12 17:47:14 +0800

    仪表盘占位

--- lanb-wvs-fe/src/page/Dashboard.vue
+++ lanb-wvs-fe/src/page/Dashboard.vue
@@ -24,13 +24,13 @@
       <div class="col-4">
         <div class="panel">
           <h1>图表2</h1>
-          <div id="echart2" style="height: 350px;"></div>
+          <div id="echart2" style="height: 350px; width: 100%;"></div>
         </div>
       </div>
       <div class="col-4">
         <div class="panel">
           <h1>图表3</h1>
-          <div id="echart3" style="height: 350px;"></div>
+          <div id="echart3" style="height: 350px; width: 100%;"></div>
         </div>
       </div>
     </div>
@@ -53,72 +53,10 @@ export default {
   },
 
   mounted () {
-    var data = [{
-      item: '事例一',
-      count: 40,
-      percent: 0.4
-    }, {
-      item: '事例二',
-      count: 21,
-      percent: 0.21
-    }, {
-      item: '事例三',
-      count: 17,
-      percent: 0.17
-    }, {
-      item: '事例四',
-      count: 13,
-      percent: 0.13
-    }, {
-      item: '事例五',
-      count: 9,
-      percent: 0.09
-    }]
-    var chart = new G2.Chart({
-      container: 'echart1',
-      forceFit: true,
-      height: 350,
-      animate: false
-    })
-    chart.source(data, {
-      percent: {
-        formatter: function formatter (val) {
-          val = val * 100 + '%'
-          return val
-        }
-      }
-    })
-    chart.coord('theta', {
-      radius: 0.75,
-      innerRadius: 0.6
-    })
-    chart.tooltip({
-      showTitle: false,
-      itemTpl: '<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
-    })
-    // 辅助文本
-    chart.guide().html({
-      position: ['50%', '50%'],
-      html: '<div style="color:#8c8c8c;font-size: 14px;text-align: center;width: 10em;">主机<br><span style="color:#8c8c8c;font-size:20px">200</span>台</div>',
-      alignX: 'middle',
-      alignY: 'middle'
-    })
-    var interval = chart.intervalStack().position('percent').color('item').label('percent', {
-      formatter: function formatter (val, item) {
-        return item.point.item + ': ' + val
-      }
-    }).tooltip('item*percent', function (item, percent) {
-      percent = percent * 100 + '%'
-      return {
-        name: item,
-        value: percent
-      }
-    }).style({
-      lineWidth: 1,
-      stroke: '#fff'
-    })
-    chart.render()
-    interval.setSelected(data[0])
+    // 初始化图表
+    this.renderCharts('echart1')
+    this.renderCharts('echart2')
+    this.renderCharts('echart3')
 
     // 取首页仪表盘数据
     const parms = {
@@ -136,6 +74,77 @@ export default {
           path: '/login'
         })
       })
+  },
+
+  methods: {
+    renderCharts (id) {
+      var data = [{
+        item: '事例一',
+        count: 40,
+        percent: 0.4
+      }, {
+        item: '事例二',
+        count: 21,
+        percent: 0.21
+      }, {
+        item: '事例三',
+        count: 17,
+        percent: 0.17
+      }, {
+        item: '事例四',
+        count: 13,
+        percent: 0.13
+      }, {
+        item: '事例五',
+        count: 9,
+        percent: 0.09
+      }]
+      var chart = new G2.Chart({
+        container: id,
+        forceFit: true,
+        height: 350,
+        animate: false
+      })
+      chart.source(data, {
+        percent: {
+          formatter: function formatter (val) {
+            val = val * 100 + '%'
+            return val
+          }
+        }
+      })
+      chart.coord('theta', {
+        radius: 0.75,
+        innerRadius: 0.6
+      })
+      chart.tooltip({
+        showTitle: false,
+        itemTpl: '<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
+      })
+      // 辅助文本
+      chart.guide().html({
+        position: ['50%', '50%'],
+        html: '<div style="color:#8c8c8c;font-size: 14px;text-align: center;width: 10em;">主机<br><span style="color:#8c8c8c;font-size:20px">200</span>台</div>',
+        alignX: 'middle',
+        alignY: 'middle'
+      })
+      var interval = chart.intervalStack().position('percent').color('item').label('percent', {
+        formatter: function formatter (val, item) {
+          return item.point.item + ': ' + val
+        }
+      }).tooltip('item*percent', function (item, percent) {
+        percent = percent * 100 + '%'
+        return {
+          name: item,
+          value: percent
+        }
+      }).style({
+        lineWidth: 1,
+        stroke: '#fff'
+      })
+      chart.render()
+      interval.setSelected(data[0])
+    }
   }
 }
 </script>
